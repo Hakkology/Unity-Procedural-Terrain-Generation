8eb1546eac59887f9f5faf71d8ea9b1169acedf9
Hakkology <yildizhakan88@gmail.com>
2025-10-30 18:05:38 +0300

    midpoint

--- Assets/Scripts/Editor/MidpointTerrainEditor.cs
+++ Assets/Scripts/Editor/MidpointTerrainEditor.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class MidpointTerrainEditor : BaseTerrainEditor
 {
     // SerializedProperty randomHeightRange;
+    SerializedProperty MPDHeightMin;
+    SerializedProperty MPDHeightMax;
+    SerializedProperty MPDHeightDampner;
+    SerializedProperty MPDRoughness;
     bool midpoint = true;
 
     protected override void OnEnable()
     {
         base.OnEnable();
         // randomHeightRange = serializedObject.FindProperty("randomHeightRange");
+        MPDHeightMin = serializedObject.FindProperty("MPDHeightMin");
+        MPDHeightMax = serializedObject.FindProperty("MPDHeightMax");
+        MPDHeightDampner = serializedObject.FindProperty("MPDHeightDampner");
+        MPDRoughness = serializedObject.FindProperty("MPDRoughness");
     }
 
     protected override void DrawTerrainParameters()
@@ -20,7 +28,13 @@ public class MidpointTerrainEditor : BaseTerrainEditor
         midpoint = EditorGUILayout.Foldout(midpoint, "Midpoint Parameters");
         if (midpoint)
         {
-            // EditorGUILayout.PropertyField(randomHeightRange);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Midpoint Displacement", EditorStyles.boldLabel);
+
+            EditorGUILayout.Slider(MPDHeightMin, -10f, 0f, new GUIContent("Height Min"));
+            EditorGUILayout.Slider(MPDHeightMax, 0f, 10f, new GUIContent("Height Max"));
+            EditorGUILayout.Slider(MPDHeightDampner, 0.1f, 10f, new GUIContent("Height Dampner"));
+            EditorGUILayout.Slider(MPDRoughness, 0.1f, 10f, new GUIContent("Roughness"));
         }
     }
 }
--- Assets/Scripts/Terrain/MidpointTerrain.cs
+++ Assets/Scripts/Terrain/MidpointTerrain.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class MidpointTerrain : BaseTerrain
 {
+    public float MPDHeightMin = -2.0f;
+    public float MPDHeightMax = 2.0f;
+    public float MPDHeightDampner = 2.0f;
+    public float MPDRoughness = 2.0f;
 
     public override void GenerateTerrain()
     {
         GenerateMidpointDisplacementTerrain();
-
     }
 
     public void GenerateMidpointDisplacementTerrain()
@@ -14,30 +17,91 @@ public class MidpointTerrain : BaseTerrain
         float[,] heightMap = GetHeightMap();
         int width = heightMapRes - 1;
         int squareSize = width;
+        float heightMin = MPDHeightMin;
+        float heightMax = MPDHeightMax;
+        float heightDampener = (float)Mathf.Pow(MPDHeightDampner, -1 * MPDRoughness);
 
         int cornerX, cornerY;
         int midX, midY;
-        int pmiXL, pmidXR, pmidYU, pmidYD;
+        int pmidXL, pmidXR, pmidYU, pmidYD;
 
-        heightMap[0, 0] = Random.Range(0, .2f);
-        heightMap[0, heightMapRes - 1] = Random.Range(0, .2f);
-        heightMap[heightMapRes - 1, 0] = Random.Range(0, .2f);
-        heightMap[heightMapRes - 1, heightMapRes - 1] = Random.Range(0, .2f);
+        //heightMap[0, 0] = UnityEngine.Random.Range(0.0f, 0.2f);
+        //heightMap[0, hmr - 2] = UnityEngine.Random.Range(0.0f, 0.2f);
+        //heightMap[hmr - 2, 0] = UnityEngine.Random.Range(0.0f, 0.2f);
+        //heightMap[hmr - 2, hmr - 2] = UnityEngine.Random.Range(0.0f, 0.2f);
 
-        for (int x = 0; x < width; x += squareSize)
-        {
-            for (int y = 0; y < width; y += squareSize)
-            {
-                cornerX = (x + squareSize);
-                cornerY = (y + squareSize);
+        while (squareSize > 0) {
 
-                midX = (int)(x + squareSize / 2.0f);
-                midY = (int)(y + squareSize / 2.0f);
+            for (int x = 0; x < width; x += squareSize) {
 
-                heightMap[midX, midY] = (float)((heightMap[x, y] + heightMap[cornerX, y] + heightMap[x, cornerY] + heightMap[cornerX, cornerY]) / 4.0f);
+                for (int y = 0; y < width; y += squareSize) {
+
+                    cornerX = (x + squareSize);
+                    cornerY = (y + squareSize);
+
+                    midX = (int)(x + squareSize / 2.0f);
+                    midY = (int)(y + squareSize / 2.0f);
+
+                    heightMap[midX, midY] = (float)((heightMap[x, y] +
+                                                     heightMap[cornerX, y] +
+                                                     heightMap[x, cornerY] +
+                                                     heightMap[cornerX, cornerY]) / 4.0f +
+                                                     Random.Range(heightMin, heightMax));
+                }
             }
-        }
 
+            for (int x = 0; x < width; x += squareSize) {
+
+                for (int y = 0; y < width; y += squareSize) {
+
+                    cornerX = (x + squareSize);
+                    cornerY = (y + squareSize);
+
+                    midX = (int)(x + squareSize / 2.0f);
+                    midY = (int)(y + squareSize / 2.0f);
+
+                    pmidXR = (int)(midX + squareSize);
+                    pmidYU = (int)(midY + squareSize);
+                    pmidXL = (int)(midX - squareSize);
+                    pmidYD = (int)(midY - squareSize);
+
+                    if (pmidXL <= 0 || pmidYD <= 0
+                        || pmidXR >= width - 1 || pmidYU >= width - 1) continue;
+
+                    // Calculate the square value for the bottom right
+                    heightMap[midX, y] = (float)((heightMap[midX, midY] +
+                                                  heightMap[x, y] +
+                                                  heightMap[midX, pmidYD] +
+                                                  heightMap[cornerX, y]) / 4.0f +
+                                                  Random.Range(heightMin, heightMax));
+
+                    // Calculate the square value for the top side
+                    heightMap[midX, cornerY] = (float)((heightMap[x, cornerY] +
+                                                  heightMap[midX, midY] +
+                                                  heightMap[cornerX, cornerY] +
+                                                  heightMap[midX, pmidYU]) / 4.0f +
+                                                  Random.Range(heightMin, heightMax));
+
+                    // Calculate the square value for the left side
+                    heightMap[x, midY] = (float)((heightMap[x, y] +
+                                                  heightMap[pmidXL, midY] +
+                                                  heightMap[x, cornerY] +
+                                                  heightMap[midX, midY]) / 4.0f +
+                                                  Random.Range(heightMin, heightMax));
+
+                    // Calculate the square value for the right side
+                    heightMap[cornerX, midY] = (float)((heightMap[midX, y] +
+                                                  heightMap[midX, midY] +
+                                                  heightMap[cornerX, cornerY] +
+                                                  heightMap[pmidXR, midY]) / 4.0f +
+                                                  Random.Range(heightMin, heightMax));
+                }
+            }
+
+            squareSize = (int)(squareSize / 2.0f);
+            heightMin *= heightDampener;
+            heightMax *= heightDampener;
+        }
         terrainData.SetHeights(0, 0, heightMap);
     }
 }
